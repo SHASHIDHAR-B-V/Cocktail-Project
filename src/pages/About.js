8d08813588bac08906fb3176922b2efc89b1529d
unknown <mr.shashidhar30@gmail.com>
2022-07-26 13:27:42 +0530

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h2 className='section-title'>about us</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. A, porro iste
+        illum laborum sint fugiat odit reiciendis cum et. Sapiente. Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Praesentium, labore itaque
+        perferendis, hic harum illum distinctio consectetur temporibus placeat
+        culpa modi, voluptatum dolorum autem facere aut officia iure neque.
+        Tempora repudiandae tenetur fuga praesentium, voluptatibus delectus
+        earum?
+      </p>
+    </section>
+  );
+};
+
+export default About;
